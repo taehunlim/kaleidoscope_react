3305e3c235fcbd35b9e412bdacc10137167c07e5
taehunlim <dsa1520@naver.com>
2022-09-04 20:26:34 +0900

    Fix: slide button not clicked

--- src/components/KaleidoscopeSlide/index.tsx
+++ src/components/KaleidoscopeSlide/index.tsx
@@ -45,16 +45,15 @@ const ButtonContainer = styled.div`
 `;
 
 const KaleidoscopeWrapper = styled.div`
-  width: 100%;
-  height: 100%;
   position: absolute;
   
   top: 0;
+  bottom: 0;
   left: 0;
-  
+  right: 0;
   margin: auto;
 
-  display: flex;
+  display: inline-table;
   justify-content: center;
   text-align: center;
 `;
