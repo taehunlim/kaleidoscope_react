6b86a6e0c8a65b26a0b7092bfcb14dd3ba084d98
taehunlim <dsa1520@naver.com>
2022-09-14 22:10:23 +0900

    Feat: Modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import KaleidoscopeSlide from "../components/KaleidoscopeSlide";
 import Templates from "components/templates";
+import Modal from "components/Modal";
 
-import imagesJson from 'images.json';
+import imagesJson from "images.json";
 
 const Home = () => {
-    return (
-        <Templates>
-            <KaleidoscopeSlide images={imagesJson} onChange={e => console.log(e)}/>
-        </Templates>
-    );
+  const [isShow, setIsShow] = useState<boolean>();
+  return (
+    <Templates>
+      <KaleidoscopeSlide images={imagesJson} onChange={(e) => console.log(e)} />
+      <Modal show={isShow} onClose={() => setIsShow(false)}></Modal>
+    </Templates>
+  );
 };
 
 export default Home;
